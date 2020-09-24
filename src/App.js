2e5d315b7803cd4a5d65eec1efdd5ea9e1753588
abir yusuf <abiryusuf@abirs-imac.home>
2020-09-24 01:15:56 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
-     
+      <Header></Header>
     </div>
   );
 }
